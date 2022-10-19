f417b0e71607c6d561de9a3ed6b54258104e1227
Michael Bianco <iloveitaly@gmail.com>
2022-10-19 14:38:17 +0000

    post install comments

--- asdf-post-install.sh
+++ asdf-post-install.sh
@@ -2,11 +2,13 @@
 
 cd "${0%/*}/.."
 
+# when running in a codespace, this should always be set
 if [ -z "$CODESPACE_VSCODE_FOLDER" ]; then
   echo "CODESPACE_VSCODE_FOLDER is not defined, defaulting to $HOME"
   CODESPACE_VSCODE_FOLDER="$HOME"
 fi
 
+# find all .tool-versions within the repo, but ignore all hidden directories
 /bin/find $CODESPACE_VSCODE_FOLDER -type d -path '*/.*' -prune -o -name '*.tool-version*' -print | while read filePath; do
   echo "asdf setup for $filePath"
 
@@ -17,6 +19,7 @@ fi
   (cd $(dirname $filePath) && asdf install)
 done
 
+# automatically startup a docker-compose that exists in the devcontainer folder
 if test -f $CODESPACE_VSCODE_FOLDER/.devcontainer/docker-compose.yml; then
   docker compose up -d
 fi

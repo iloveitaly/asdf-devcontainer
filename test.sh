8f3e20227518a4a1915f29aef6a50c24be444d3c
Michael Bianco <iloveitaly@gmail.com>
2022-10-19 15:57:32 +0000

    Hacky test file

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+docker run --rm -it asdf-devcontainer bash -lc "echo 'python 3.10.8' > ~/.tool-versions && /asdf-post-install.sh && asdf reshim && python --version"
+
+docker run --rm -it asdf-devcontainer bash -lc "echo 'php latest' > ~/.tool-versions && /asdf-post-install.sh && php --version"
+
+docker run --rm -it asdf-devcontainer bash -lc "echo 'ruby 3.1.2' > ~/.tool-versions && /asdf-post-install.sh && asdf reshim && ruby --version"
+
+docker run --rm -it asdf-devcontainer bash -lc "echo 'nodejs 18.11.0' > ~/.tool-versions && /asdf-post-install.sh && asdf reshim && node --version"
+
+docker run --rm -it asdf-devcontainer bash -lc "echo 'bun 0.2.1' > ~/.tool-versions && /asdf-post-install.sh && asdf reshim && bun --version"
+
+docker run --rm -it asdf-devcontainer bash -lc "echo 'deno 1.26.2' > ~/.tool-versions && /asdf-post-install.sh && asdf reshim && deno --version"
+
+docker run --rm -it asdf-devcontainer bash -lc "echo -e 'elixir latest\nerlang latest' > ~/.tool-versions && /asdf-post-install.sh && asdf reshim && elixir --version"
